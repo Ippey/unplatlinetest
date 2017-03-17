e3a82e8c93d04707826386cb6a3bb8748e41fa3a
Ippei Sumida <ippey.s@gmail.com>
2017-03-17 14:19:34 +0900

    use MessageBuilder

--- sample_webhook.php
+++ sample_webhook.php
@@ -10,6 +10,7 @@ require 'vendor/autoload.php';
 
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 $httpClient = new CurlHTTPClient('w9zq+jacy2Eu5Zmzx0cpvLCx6ja7uwz2NjtATmYJZC8wufox3p9pvd8im+Ad9A9zLWjMTsvZrG68C6qzAkaCQ7G/QblGPDE8Y+8nAKkcClYkeNgdWCwo5eMqOa1U2/n7bvkiEKA0LucC3mkXcCDWZgdB04t89/1O/w1cDnyilFU=');
 $bot = new LINEBot($httpClient, ['channelSecret' => '71607df983dec564a66e74ac8018f679']);
@@ -24,4 +25,5 @@ $text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
 //ReplyToken取得
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
-$bot->replyMessage($replyToken, 'hello world.');
+$textMessageBuilder = new TextMessageBuilder('hello world');
+$bot->replyMessage($replyToken, $textMessageBuilder);
