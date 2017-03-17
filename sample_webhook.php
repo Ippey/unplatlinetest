a9831fa94bc67b4fe4e6cf3d2187c52468084931
Ippei Sumida <ippey.s@gmail.com>
2017-03-17 13:56:46 +0900

    update sample_webhook.php

--- sample_webhook.php
+++ sample_webhook.php
@@ -6,7 +6,12 @@
  * Time: 15:28
  */
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('w9zq+jacy2Eu5Zmzx0cpvLCx6ja7uwz2NjtATmYJZC8wufox3p9pvd8im+Ad9A9zLWjMTsvZrG68C6qzAkaCQ7G/QblGPDE8Y+8nAKkcClYkeNgdWCwo5eMqOa1U2/n7bvkiEKA0LucC3mkXcCDWZgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '71607df983dec564a66e74ac8018f679']);
+require 'vendor/autoload.php';
+
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
+
+$httpClient = new CurlHTTPClient('w9zq+jacy2Eu5Zmzx0cpvLCx6ja7uwz2NjtATmYJZC8wufox3p9pvd8im+Ad9A9zLWjMTsvZrG68C6qzAkaCQ7G/QblGPDE8Y+8nAKkcClYkeNgdWCwo5eMqOa1U2/n7bvkiEKA0LucC3mkXcCDWZgdB04t89/1O/w1cDnyilFU=');
+$bot = new LINEBot($httpClient, ['channelSecret' => '71607df983dec564a66e74ac8018f679']);
 
 $bot->replyMessage('hello world.');
