7e13ce82ac2079d26cc3adbf313b8253dfc61ac7
Ippei Sumida <ippey.s@gmail.com>
2017-03-17 14:08:30 +0900

    bugfix?

--- sample_webhook.php
+++ sample_webhook.php
@@ -14,4 +14,14 @@ use LINE\LINEBot;
 $httpClient = new CurlHTTPClient('w9zq+jacy2Eu5Zmzx0cpvLCx6ja7uwz2NjtATmYJZC8wufox3p9pvd8im+Ad9A9zLWjMTsvZrG68C6qzAkaCQ7G/QblGPDE8Y+8nAKkcClYkeNgdWCwo5eMqOa1U2/n7bvkiEKA0LucC3mkXcCDWZgdB04t89/1O/w1cDnyilFU=');
 $bot = new LINEBot($httpClient, ['channelSecret' => '71607df983dec564a66e74ac8018f679']);
 
-$bot->replyMessage('hello world.');
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+
+$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+//メッセージ取得
+$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+//ReplyToken取得
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+$bot->replyMessage($replyToken, 'hello world.');
